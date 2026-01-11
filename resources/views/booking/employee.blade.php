e705dd9261b9ef3f8c09688a0d4a15fdf8f3dc28
Naderadelp <naderadelpp@gmail.com>
2026-01-11 14:24:21 +0200

    Enhance employee booking view with service details
    - Updated the employee booking view to improve the presentation of services.
    - Added service price and duration information below each service title for better clarity.
    - Adjusted layout with additional margin for improved spacing.

--- resources/views/booking/employee.blade.php
+++ resources/views/booking/employee.blade.php
@@ -2,14 +2,17 @@
 
     <a href="{{ route('booking') }}" class="text-xs text-blue-500">&larr; Go Back</a>
 
-    <h1 class="text-2xl font-medium">
+    <h1 class="text-2xl font-medium mt-6">
         Now Choosea service from {{ $employee->name }}
     </h1>
 
     <div class="grid grid-cols-2 md:grid-cols-3 gap-8 mt-6">
         @foreach ($services as $service)
         <a href="#" class="py-8 px-4 border border-slate-200 rounded-lg shadow-sm flex flex-col items-center justify-center text-center hover:bg-gray-50/75">
-            {{ $service->title }}
+            <div class="text-sm font-medium text-slate-600">{{ $service->title }}</div>
+            <div class="text-sm font-medium text-slate-400 mt-1">{{ $service->price }}</div>
+            <div class="text-sm font-medium text-slate-400 mt-2">{{ $service->duration }} minutes</div>
+
         </a>
         @endforeach
     </div>
